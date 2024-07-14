81d78178a294ced6d65704ca660affed6fabce1b
ReneVajda1 <rene.vajda@yahoo.com>
2024-07-15 01:50:57 +0200

    feat: adding HealthController

---
+++ BackendAPI/src/main/java/backendapi/backendapi/HealthController.java
@@ -0,0 +1,14 @@
+package backendapi.backendapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
