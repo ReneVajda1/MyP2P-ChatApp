cf9f894d65a27182d1cc9590169574e1c3d9f8f5
ReneVajda1 <rene.vajda@yahoo.com>
2024-07-15 02:05:06 +0200

    controller package created

--- BackendAPI/src/main/java/backendapi/backendapi/controllers/HealthController.java
+++ BackendAPI/src/main/java/backendapi/backendapi/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package backendapi.backendapi;
+package backendapi.backendapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
